6b32c53b525d5ddf01ac1c61b5a86ebeb551b10c
Keerthana31196 <keerthana.v@agilisium.com>
2021-04-21 08:26:26 +0530

    Changes in urlconfig file

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -2,10 +2,13 @@ let urlConfig = {
     backend: "https://f2w5o7vsrc.execute-api.us-east-2.amazonaws.com"
 }
 
-urlConfig.RatingGetAPI = "https://f2w5o7vsrc.execute-api.us-east-2.amazonaws.com/alpha/rating?entity=Example%20Burger";
-urlConfig.RestaurantFoodGetAPI = "https://f2w5o7vsrc.execute-api.us-east-2.amazonaws.com/alpha/restaurant/food?restaurant_name=";
-urlConfig.RestaurantByFilterGetAPI = "https://f2w5o7vsrc.execute-api.us-east-2.amazonaws.com/alpha/restaurant/";
-urlConfig.OrderPostAPI = "https://f2w5o7vsrc.execute-api.us-east-2.amazonaws.com/alpha/order";
-urlConfig.RatingPostAPI = "https://f2w5o7vsrc.execute-api.us-east-2.amazonaws.com/alpha/rating";
+urlConfig.RatingGetAPI = urlConfig.backend + "/alpha/rating?entity=Example%20Burger";
+urlConfig.RestaurantFoodGetAPI = urlConfig.backend + "/alpha/restaurant/food?restaurant_name=";
+urlConfig.RestaurantByFilterGetAPI = urlConfig.backend + "/alpha/restaurant/";
+urlConfig.RestaurantByPriceGetAPI = urlConfig.backend + "/alpha/restaurant/filter";
+urlConfig.RestaurantByRatingGetAPI = urlConfig.backend + "/alpha/restaurant/rating";
+urlConfig.OrderPostAPI = urlConfig.backend + "/alpha/order";
+urlConfig.RatingPostAPI = urlConfig.backend + "/alpha/rating";
+urlConfig.SearchPostAPI = urlConfig.backend + "/alpha/search";
 
 export default urlConfig;
