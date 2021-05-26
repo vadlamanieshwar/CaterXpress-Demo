11b5b7c22afd2596639deae883c2c12277308f9d
Ashley <69605871+AshleyYt@users.noreply.github.com>
2021-05-25 23:05:20 -0700

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -5,8 +5,8 @@ let urlConfig = {
 urlConfig.RatingGetAPI = urlConfig.backend + "/alpha/rating?entity=Example%20Burger";
 urlConfig.RestaurantFoodGetAPI = urlConfig.backend + "/alpha/restaurant/food?restaurant_name=";
 urlConfig.RestaurantByFilterGetAPI = urlConfig.backend + "/alpha/restaurant/";
-urlConfig.RestaurantByPriceGetAPI = urlConfig.backend + "/alpha/restaurant/by-price"
-urlConfig.RestaurantByRatingGetAPI = urlConfig.backend + "/alpha/restaurant/by-rating"
+urlConfig.RestaurantByPriceGetAPI = urlConfig.backend + "/alpha/restaurant/by_price"
+urlConfig.RestaurantByRatingGetAPI = urlConfig.backend + "/alpha/restaurant/by_rating"
 urlConfig.OrderPostAPI = urlConfig.backend + "/alpha/order";
 urlConfig.RatingPostAPI = urlConfig.backend + "/alpha/rating";
 urlConfig.SearchPostAPI = urlConfig.backend + "/alpha/restaurant/search?prefix=";
